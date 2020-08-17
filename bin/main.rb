68bb6bfaa144ed2932cc1cc296a4849f234d9478
haywhizzz <haywhizzz@gmail.com>
2020-08-17 11:05:17 +0100

    Add condition to logic

--- bin/main.rb
+++ bin/main.rb
@@ -24,3 +24,11 @@ end
 def player_move(board, index, marker)
   board[index] = marker
 end
+
+def position_taken?(board, index)
+  if board[index] == '' || board[index] == ' ' || board[index].nil?
+    false
+  else
+    true
+  end
+end
