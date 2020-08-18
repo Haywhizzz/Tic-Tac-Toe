870b626d3aa1c04762223d3cd2be46dfa73e2288
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-18 14:10:11 +0300

    Pritave class added

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,20 @@
 class Gameplay
+  attr_accessor :winning_combinations
+  private :winning_combinations
+  def initialize
     winning_combinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-    
-    def won?(board)
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+  end
+
+  def won?(board)
     winning_combinations.each do |win_combo|
       win_index_1 = win_combo[0]
       win_index_2 = win_combo[1]
@@ -20,11 +24,13 @@ class Gameplay
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
 
-      return win_combo if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+        return win_combo
+      else
+        return false
+      end
     end
-    end
-    false
-    end
+  end
 
   def full?(board)
     if board.any? { |index| index.nil? || index == ' ' }
@@ -51,6 +57,4 @@ class Gameplay
       false
     end
   end
-  
-end
-
+  end
