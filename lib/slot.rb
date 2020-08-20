b6704331734280858bad68e890aa688725edaa47
haywhizzz <haywhizzz@gmail.com>
2020-08-20 15:52:39 +0100

    Fix rubocop errors

--- lib/slot.rb
+++ lib/slot.rb
@@ -1,8 +1,4 @@
-# [[1,2,3],
-#  [4,5,6],
-#  [7,8,9]]
-#  [[1,2,3], [4,5,6], [7,8,9]].flatten == [1,2,3,4,5,6,7,8,9].map(&:value)
-#  [[1,2,3], [4,5,6], [7,8,9]] + [[1,2,3], [4,5,6], [7,8,9]].transpose [[1,4,7], [2,5,8], [3,6,9]]
+# rubocop: disable Style/ClassVars
 class Slot
   attr_accessor :value
   @@count = 1
@@ -11,3 +7,4 @@ class Slot
     @@count += 1
   end
 end
+# rubocop: enable Style/ClassVars
