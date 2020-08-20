b349fab600d3e200a9c55b6d20547e288c37a7f2
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-20 14:59:57 +0300

    Fix rubocop errors

--- lib/board.rb
+++ lib/board.rb
@@ -3,18 +3,21 @@ require_relative './array_extensions'
 class Board
   attr_reader :board
   def initialize
-    @board = Array.new(3){Array.new(3){Slot.new}}
+    @board = Array.new(3) { Array.new(3) { Slot.new } }
   end
+
   def get_a_board_slot(x_coord, y_coord)
-      board[x_coord][y_coord]
+    board[x_coord][y_coord]
   end
+
   def set_a_board_slot(x_coord, y_coord, value)
-    if get_a_board_slot(x_coord,y_coord).value.to_i.positive?
-      get_a_board_slot(x_coord,y_coord).value = value
+    if get_a_board_slot(x_coord, y_coord).value.to_i.positive?
+      get_a_board_slot(x_coord, y_coord).value = value
     else
       false
     end
   end
+
   def display_board
     displayed_board = []
     board.each do |array|
@@ -22,26 +25,33 @@ class Board
     end
     displayed_board.each { |array| p array }
   end
+
   def game_ended?
     return :win if won?
     return :draw if drawn?
+
     false
   end
+
   def drawn?
     board.flatten.map(&:value).no_cell_empty?
   end
+
   def board_diagonals
     [
       [get_a_board_slot(0, 0), get_a_board_slot(1, 1), get_a_board_slot(2, 2)],
       [get_a_board_slot(0, 2), get_a_board_slot(1, 1), get_a_board_slot(2, 0)]
     ]
   end
+
   def winning_config
     board + board.transpose + board_diagonals
   end
+
   def winning_values(winning_config)
     winning_config.map(&:value)
   end
+
   def won?
     winning_config.each do |config|
       next if winning_values(config).all_cells_empty?
--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,23 @@
 class Gameplay
   attr_accessor :WINNING
   private :WINNING
-  def initialize
-   
-  end
-   WINNING = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
+  def initialize; end
+  WINNING = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
 
   def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_combination.all? { |win_index| board[win_index] == "X" } ||
-    win_combination.all? { |win_index| board[win_index] == "O" }
-  end
+    WIN_COMBINATIONS.detect do |win_combination|
+      win_combination.all? { |win_index| board[win_index] == 'X' } ||
+        win_combination.all? { |win_index| board[win_index] == 'O' }
+    end
   end
 
   def full?(board)
