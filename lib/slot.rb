fa9a54872237567b7db3a9f760a3a182c5a40f1f
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-20 14:42:49 +0300

    Create slot.rb file

---
+++ lib/slot.rb
@@ -0,0 +1,16 @@
+=begin
+[[1,2,3],
+ [4,5,6],
+ [7,8,9]]
+ [[1,2,3], [4,5,6], [7,8,9]].flatten == [1,2,3,4,5,6,7,8,9].map(&:value)
+ [[1,2,3], [4,5,6], [7,8,9]] + [[1,2,3], [4,5,6], [7,8,9]].transpose [[1,4,7], [2,5,8], [3,6,9]]
+=end
+class Slot
+    attr_accessor :value
+    @@count = 1
+    def initialize
+      @value = @@count
+      @@count += 1
+    end
+  end
+  
