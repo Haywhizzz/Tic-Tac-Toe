f84e817b86a10d6d6cb946b2fb340e9ce17fb3fe
haywhizzz <haywhizzz@gmail.com>
2020-08-20 15:17:32 +0100

    Add board_position_mapping method

--- lib/game.rb
+++ lib/game.rb
@@ -29,4 +29,20 @@ class Game
     end
   end
 
+  private
+  def board_position_mapping(player_move)
+    board_positions = { 1 => [0, 0],
+                        2 => [0, 1],
+                        3 => [0, 2],
+                        4 => [1, 0],
+                        5 => [1, 1],
+                        6 => [1, 2],
+                        7 => [2, 0],
+                        8 => [2, 1],
+                        9 => [2, 2] }
+    board_positions[player_move]
+  end
+end
+
+
 
