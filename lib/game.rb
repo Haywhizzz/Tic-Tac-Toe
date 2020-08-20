a568223e4a957aceb7e7f3d50651c9dbdc783c4b
haywhizzz <haywhizzz@gmail.com>
2020-08-20 13:43:57 +0100

    Add game class and initialize definition

--- lib/game.rb
+++ lib/game.rb
@@ -1,48 +1,11 @@
-class Gameplay
-  attr_accessor :WINNING
-  private :WINNING
-  def initialize; end
-  WINNING = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ].freeze
+require_relative './player'
+require_relative './array_extensions'
+require_relative './board'
 
-  def won?(board)
-    WIN_COMBINATIONS.detect do |win_combination|
-      win_combination.all? { |win_index| board[win_index] == 'X' } ||
-        win_combination.all? { |win_index| board[win_index] == 'O' }
-    end
+class Game
+  attr_accessor :board, :current_player, :second_player
+  def initialize(board=Board.new, player_one, player_two)
+    @current_player = player_one
+    @second_player = player_two
+    @board = board
   end
-
-  def full?(board)
-    if board.any? { |index| index.nil? || index == ' ' }
-      false
-    else
-      true
-    end
-  end
-
-  def draw?(board)
-    if !won?(board) && full?(board)
-      true
-    elsif !full?(board) && !won?(board)
-      false
-    else won?(board)
-         false
-    end
-  end
-
-  def over?(board)
-    if draw?(board) || won?(board) || full?(board)
-      true
-    else
-      false
-    end
-  end
-end
