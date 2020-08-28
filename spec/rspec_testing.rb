a297aea2f33adf3007ca0d684b279f4bc8a49d28
haywhizzz <haywhizzz@gmail.com>
2020-08-28 11:21:25 +0100

    Add player method

---
+++ spec/rspec_testing.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/player'
+
+describe Player do  
+    let(:player) { Player.new('Cris', 'X') }
+
+
+  describe '#name' do    
+    it 'returns the name of the player' do      
+    expect(player.name).to eql('Cris')    
+    end  
+  end
+
+  describe '#sign' do   
+     it 'returns the sign of the player' do      
+    expect(player.symbol).to eql('X')    
+     end  
+  end
+end
